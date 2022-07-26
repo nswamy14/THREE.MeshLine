add80e0b80c9deadf6d0388f6edfe65466766648
Narayana Swamy <narayanaswamy14@gmail.com>
2022-07-26 11:32:49 +0530

    Meshline changes

--- src/THREE.MeshLine.js
+++ src/THREE.MeshLine.js
@@ -59,7 +59,8 @@
 				this.counters[j * 2 + 1] = c;
 				// this.counters.push(c);
 				// this.counters.push(c);
-				let o = (j < (l / 3)) ? ((3 / l) * j) : (j > (3 * l / 4) ? ((3 / l) * (3 * (l / 4) - j) + 0.5) * 2 :  1);
+				// let o = (j < (l / 3)) ? ((3 / l) * j) : (j > (3 * l / 4) ? ((3 / l) * (3 * (l / 4) - j) + 0.5) * 2 :  1);
+				let o = (j < 20) ? (0.05 * j) : (j > (l - 20) ? (0.05 * (l - j)) : 1);
 				o = o || 0.01;
 				o = this.useDash ? 1.0 : o;
 				// this.opacity.push( o );
@@ -90,7 +91,8 @@
 				this.counters[j * 2 + 1] = c;
 				// this.counters.push(c);
 				// this.counters.push(c);
-				let o = (j < (l / 3)) ? ((3 / l) * j) : (j > (3 * l / 4) ? ((3 / l) * (3 * (l / 4) - j) + 0.5) * 2 :  1);
+				// let o = (j < (l / 3)) ? ((3 / l) * j) : (j > (3 * l / 4) ? ((3 / l) * (3 * (l / 4) - j) + 0.5) * 2 :  1);
+				let o = (j < 20) ? (0.05 * j) : (j > (l - 20) ? (0.05 * (l - j)) : 1);
 				o = o || 0.01;
 				o = this.useDash ? 1.0 : o;
 				// this.opacity.push( o );
